10789b43f113e7c80d872576795b31c46b259f84
thayl0w <softmardevelopment@gmail.com>
2025-09-14 19:32:07 +0800

    change

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
-// Use .setup directly with both serve + setup
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
